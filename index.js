0844a8abd3d5922f699c586d3e4dfdf04b1612c8
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-02-06 17:00:54 -0600

    this gets the correct answer

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-async function fetchJSON(fetchPath) {
+async function getCorrectAnswer(fetchPath) {
   const path = window.location.href;
   const questionNumber = parseInt(path.split('%20')[1]);
   const data = await fetch(fetchPath);
@@ -6,5 +6,5 @@ async function fetchJSON(fetchPath) {
   const json = {...result[0]};
   const filteredQuestions = json.pages.filter(item => item.templateName === 'Question');
   const currentQuestion = filteredQuestions.filter(question => question.contents[0].settings[0].value === questionNumber)
-  return currentQuestion;
+  return currentQuestion[0].contents.filter((item => item.translations[1]?.value === "100"))[0].translations[2].value;
 }
